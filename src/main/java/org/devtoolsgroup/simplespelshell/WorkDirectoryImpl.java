9fcae6f895c34978ac90b043c2649e1fe3d9725c
DevToolsGroup <DevToolsGroup@example.email>
2026-08-18 18:47:50 +0200

    Implement WorkDirectoryImpl.java.

---
+++ src/main/java/org/devtoolsgroup/simplespelshell/WorkDirectoryImpl.java
@@ -0,0 +1,101 @@
+/*
+MIT License
+
+Copyright (c) 2026-present DevToolsGroup (https://github.com/DevToolsGroup)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package org.devtoolsgroup.simplespelshell;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+public class WorkDirectoryImpl implements WorkDirectory {
+    private Path curDir;
+    private Consumer<Path> currentDirectoryValidator;
+
+    @Override
+    public void changeCurrentDir(Path path) {
+        if (path.toString().isBlank()) {
+            return;
+        }
+        path = curDir.resolve(path).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new ShellException("The directory %s doesn't exist.".formatted(path));
+        }
+        if (!Files.isDirectory(path)) {
+            throw new ShellException("%s is not a directory.".formatted(path));
+        }
+        if (currentDirectoryValidator != null) {
+            currentDirectoryValidator.accept(path);
+        }
+        curDir = path;
+    }
+
+    @Override
+    public void setCurrentDirectoryValidator(Path newCurDir, Consumer<Path> validator) {
+        this.currentDirectoryValidator = null;
+        changeCurrentDir(newCurDir);
+        this.currentDirectoryValidator = validator;
+        if (currentDirectoryValidator != null) {
+            currentDirectoryValidator.accept(curDir);
+        }
+    }
+
+    @Override
+    public Path getCurDirAbsolutePath() {
+        return curDir;
+    }
+
+    @Override
+    public File getFile(Path path) {
+        return curDir.resolve(path).toAbsolutePath().normalize().toFile();
+    }
+
+    @Override
+    public void write(Path path, String text, Charset cs) {
+        File fileToWriteTo = getFile(path);
+        if (!ShellUtils.isParentChild(curDir, fileToWriteTo.toPath())) {
+            throw new ShellException("Cannot write outside of the current directory %s".formatted(curDir));
+        }
+        if (fileToWriteTo.exists() && fileToWriteTo.isDirectory()) {
+            throw new ShellException("Cannot write text to %s because it is a directory".formatted(fileToWriteTo));
+        }
+        File parentDir = fileToWriteTo.getParentFile();
+        if (parentDir.exists() || parentDir.mkdirs()) {
+            try {
+                Files.writeString(fileToWriteTo.toPath(), text, cs);
+            } catch (IOException e) {
+                throw new ShellException(e);
+            }
+        }
+        throw new ShellException("There was an error when creating parent directories %s".formatted(parentDir));
+    }
+
+    @Override
+    public void write(Path path, String text) {
+        write(path, text, StandardCharsets.UTF_8);
+    }
+}
